6e555a73a00dd67ecde546b4633d5cc3fdab8af0
shravanibhakare <2022bit048@sggs.ac.in>
2024-02-03 17:57:27 +0530

    add file to git

---
+++ ProgrammingAssignment1.java
@@ -0,0 +1,75 @@
+class SY2022bit048 {
+    public static void main(String[] args) {
+        String arr1 = "2022bit048";
+        int result1 = getValidRegistrationsCount(arr1);
+
+        if (result1 == 1) {
+            System.out.println("Registration number is valid");
+        } else {
+            System.out.println("Registration number is not valid");
+        }
+
+        String arr2 = "2022bcs073";
+        int result2 = getValidRegistrationsCount(arr2);
+
+        if (result2 == 1) {
+            System.out.println("Registration number is valid");
+        } else {
+            System.out.println("Registration number is not valid");
+        }
+
+        String arr3 = "2022bme045";
+        int result3 = getValidRegistrationsCount(arr3);
+
+        if (result3 == 1) {
+            System.out.println("Registration number is valid");
+        } else {
+            System.out.println("Registration number is not valid");
+        }
+
+        String arr4 = "2022bch035";
+        int result4 = getValidRegistrationsCount(arr4);
+
+        if (result4 == 1) {
+            System.out.println("Registration number is valid");
+        } else {
+            System.out.println("Registration number is not valid");
+        }
+
+        String arr5 = "2022bel027";
+        int result5 = getValidRegistrationsCount(arr5);
+
+        if (result5 == 1) {
+            System.out.println("Registration number is valid");
+        } else {
+            System.out.println("Registration number is not valid");
+        }
+    }
+
+    static int getValidRegistrationsCount(String arr) {
+        for (int i = 0; i < 4; i++) {
+            if (Character.isDigit(arr.charAt(i))) {
+                continue;
+            } else {
+                return 0;
+            }
+        }
+
+        if (arr.charAt(4) == 'b') {
+            if (arr.charAt(5) == 'i' || arr.charAt(5) == 'm' || arr.charAt(5) == 'c') {
+                if (arr.charAt(6) == 't' || arr.charAt(6) == 's' || arr.charAt(6) == 'e' || arr.charAt(6) == 'h') {
+                    for (int k = 7; k < 10; k++) {
+                        if (Character.isDigit(arr.charAt(k))) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
+
